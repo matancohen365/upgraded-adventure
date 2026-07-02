eb6985ab9fecac876ee04c7be34994063927c24f
Matan Cohen <matancohen365@gmail.com>
2026-07-03 02:13:26 +0300

    commit

--- public/index.php
+++ public/index.php
@@ -419,12 +419,12 @@
      ============================================================ -->
 
 <!-- ① Google Tag Manager (HEAD snippet) - loads GA4 + Google Ads -->
-<!-- REPLACE GTM-XXXXXXX with your real GTM container ID -->
+<!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-XXXXXXX');</script>
+})(window,document,'script','dataLayer','GTM-PXFMTZVD');</script>
 <!-- End Google Tag Manager -->
 
 <!-- ② Google Analytics 4 (direct) + Google Ads Conversion Tag -->
